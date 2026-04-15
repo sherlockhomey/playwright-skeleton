0bfc4e10c77454ea9339fc74a629a6df6ee498bd
Garnell <gpaulk@duck.com>
2026-04-15 12:42:35 -0700

    PUSH AGAIN

--- tests/smoke.spec.ts
+++ tests/smoke.spec.ts
@@ -6,6 +6,6 @@ test('Verify Playwright home page title', async ({ page }) => {
 
   // 2. Expect a title "to contain" a substring.
   // This is an "Assertion" - the heart of any QA test.
-  // Tiny change to see if commit > push works across the wire OTRA VEZ
+  // Tiny change to see if commit > push works across the wire OTRA VEZ. AND AGIAN
   await expect(page).toHaveTitle(/Playwright/);
 });
